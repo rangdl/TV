8873daebaaa8c66377d1fb87e7f716134237b6e2
rangdl <378694192@qq.com>
2024-08-12 13:07:06 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,3 +1,5 @@
+open_update = True
+open_use_old_result = True
 source_file = "user_demo.txt"
 final_file = "user_result.txt"
 favorite_list = [
@@ -17,18 +19,23 @@ open_online_search = False
 favorite_page_num = 5
 default_page_num = 3
 urls_limit = 15
+open_keep_all = False
 open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
-ipv_type = "ipv4"
+ipv_type = "all"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
-open_subscribe = True
+open_subscribe = False
 subscribe_urls = [
     "https://m3u.ibert.me/txt/fmml_dv6.txt",
     "https://m3u.ibert.me/txt/o_cn.txt",
     "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt",
 ]
 open_multicast = True
 region_list = ["河北"]
+open_proxy = True
+open_driver = False
+open_use_old_result = True
